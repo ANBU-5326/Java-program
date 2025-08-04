7d26108ee1649362591f8d85c58ef1cd47f3ae76
ANBU-5326 <anbu.march.05.2006@gmail.com>
2025-08-04 12:31:31 +0530

    Create Ag.java
    change the age

---
+++ Ag.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int s=19;
+        int sk=18;
+        if(s>sk){
+           System.out.println("x is younger than y");
+        }
+        else{
+            System.out.println("y is youger than x");
+        }
+    }
+}
