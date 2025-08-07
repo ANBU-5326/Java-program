48cf096597fb58f8d0f57586eeed10c571e758fb
ANBU-5326 <anbu.march.05.2006@gmail.com>
2025-08-07 10:38:20 +0530

    Create square.java
    square

---
+++ square.java
@@ -0,0 +1,19 @@
+public class Main
+{
+public static void main(String[] args) {
+  int v =2;
+   int n=5;
+   for(int i=1;i<=5;i++){
+        for (int j=0;j<=i;j++){
+         System.out.print((v*v)+" ");
+         v++;
+        }
+       
+        System.out.println("");
+       
+       
+       
+   }    
+}
+}
+
