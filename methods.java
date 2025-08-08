5caef84ae1b642c531f13e18f061901358bb57eb
ANBU-5326 <anbu.march.05.2006@gmail.com>
2025-08-08 09:52:39 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,9 @@
+class Main{
+        static void  method (int a,int b){
+        System.out.println(a+b);
+            
+        }
+        public static void main(String[]args){
+            method(100,80);
+    }
+}
