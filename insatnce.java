5601b6860f11430b24cc5749bf389d1f6f4c8039
ANBU-5326 <anbu.march.05.2006@gmail.com>
2025-08-08 14:03:47 +0530

    Create insatnce.java

---
+++ insatnce.java
@@ -0,0 +1,13 @@
+class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Main obj=new Main();
+		obj.Sample();
+	}
+	public  void Sample(){
+	    int a=10;
+	    System.out.println(a);
+	    
+	}
+}
