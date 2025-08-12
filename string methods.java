80ac97c330b1e710f41760c88953bbec722d84ea
ANBU-5326 <anbu.march.05.2006@gmail.com>
2025-08-12 10:39:06 +0530

    Create string methods.java

---
+++ string methods.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main(String[] args) {
+	    Scanner data=new Scanner(System.in);
+	    System.out.println("Enter The string value");
+	    String a=data.nextLine();
+	    char y=a.charAt(2);
+	    System.out.println(y);
+	    
+	    int x=a.codePointAt(2);
+	    System.out.println(x);
+	    
+	    int s=a.codePointBefore(2);
+	    System.out.println(s);
+	    
+	    int n=a.codePointCount(0,6);
+	    System.out.println(n);
+	    
+	    String b=data.nextLine();
+	    int u=a.compareTo(b);
+	    System.out.println(u);
+	    
+	    int g=a.compareToIgnoreCase(b);
+	    System.out.println(g);
+	    
+	    String e=a.concat(b);
+	    System.out.println(e);
+	    
+	    
+	}
+}
